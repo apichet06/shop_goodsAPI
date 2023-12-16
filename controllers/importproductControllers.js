3f62827896f1031e5ce5b41406ca9ec4a19be117
apichet06 <apichet06@gmail.com>
2023-12-17 02:26:15 +0700

    add ImportProduct

--- controllers/importproductControllers.js
+++ controllers/importproductControllers.js
@@ -1,15 +1,17 @@
 const Messages = require("../config/messages");
+const ImportProduct = require("../models/importproductModel");
 
 class ImportProductController {
 
     static async GetAll(req, res) {
-
         try {
-            res.status(200).send("Test ProductImport")
-        } catch (error) {
-            res.status(500).json({ status: Messages.error500, message: error.message });
-        }
+            const { Data } = req.body;
 
+            const product_import = await ImportProduct.GetAll(Data);
+            res.status(200).json({ status: "ok", data: product_import });
+        } catch (error) {
+            res.status(500).json({ status: Messages.error500, Messages: error.message });
+        }
     }
 
 }
--- models/importproductModel.js
+++ models/importproductModel.js
@@ -1,4 +1,27 @@
+const db = require("../config/db");
+
 class ImportProduct {
+
+    static async GetAll(data) {
+        try {
+            data = data || '';
+
+            const [result] = await db.query(
+                `SELECT *, 
+                (SELECT JSON_ARRAYAGG(JSON_OBJECT('image_file', c.image_file, 'image_date', c.image_date)) 
+                 FROM product_image c WHERE b.pro_id = c.pro_id) AS images
+                FROM product_import a 
+                INNER JOIN products b ON a.products_id = b.id 
+                WHERE b.pro_name LIKE  ?`,
+                [`%${data}%`]
+            );
+
+            return result;
+        } catch (error) {
+            console.error('Error in ProductImportModule GetAll:', error);
+            throw error;
+        }
+    }
 
 }
 
