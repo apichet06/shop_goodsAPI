17a12c88c879c02787b8d4d420deb3d4e7cce340
apichet06 <apichet06@gmail.com>
2023-12-17 02:45:39 +0700

    Upadte ImportProduct

--- routes/importproductRoutes.js
+++ routes/importproductRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const ImportProductController = require('../controllers/importproductControllers')
 const router = express.Router()
 
-router.get('/:data', ImportProductController.GetAll)
+router.get('/', ImportProductController.GetAll)
 router.post('/', ImportProductController.CreateProduct_import)
 router.delete('/:id', ImportProductController.DeleteProduct_import)
 module.exports = router
