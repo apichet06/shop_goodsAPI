3f480b2974cefa5158e0cb8aa6f3c85620ed16b8
apichet06 <apichet06@gmail.com>
2023-12-26 14:45:04 +0700

    update file uploads

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const Messages = require('../config/messages');
 require('dotenv');
-const fs = require('fs');
+const fs = require('@cyclic.sh/s3fs/promises')
 
 class auth {
     static async authenticateToken(req, res, next) {
